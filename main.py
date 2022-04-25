b9f52826f1202aa037b3475f5880d194d684eafa
Elk <39370174+ELKYang@users.noreply.github.com>
2022-04-25 20:53:42 +0800

    add license

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 
 #encoding=utf8
-# Copyright (c) 2021 PaddlePaddle Authors. All Rights Reserved.
+# Copyright (c) 2022 PaddlePaddle Authors. All Rights Reserved.
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
